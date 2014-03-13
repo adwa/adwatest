36d4072ecab39e7d775cd198602d712f626e89ec
AdrianW <adrian.wojdat@gmail.com>
2014-03-13 19:59:21 +0100

    merge

--- lang/ang.php
+++ lang/ang.php
@@ -5,11 +5,7 @@ $lang['group'] = 'Team';
 $lang['contact'] = 'Contact';
 $lang['wedidit'] = 'We have already done :';
 $lang['weare'] = 'Welcome to our website / portfolio. We are forming a group of websites.......';
-<<<<<<< HEAD
 $lang['description'] = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.';
-=======
-$lang['description'] ='';
->>>>>>> 371820a9d99af989a86952021c7207edd02ea229
 $lang['ritetoo'] ='Write to us.';
 $lang['name'] ='Name and Surname:';
 $lang['massage'] ='Message:';
