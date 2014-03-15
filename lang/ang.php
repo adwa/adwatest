31a9e61764513763f17950be82f3130e6373e09d
Wasyl <wasyl@adwmicro.pl>
2014-03-16 00:30:14 +0100

    dodaw angl text

--- lang/ang.php
+++ lang/ang.php
@@ -4,7 +4,9 @@ $lang['about'] = 'About us';
 $lang['group'] = 'Team';
 $lang['contact'] = 'Contact';
 $lang['wedidit'] = 'We have already done :';
-$lang['weare'] = 'Welcome to our website / portfolio. We are forming a group of websites.......';
+$lang['weare'] = 'Welcome to our website portfolio. We are a group of 3 webmasters who create web pages.
+We offer: Comfortable kontakt, Profesional solutions, low prices, coding to W3C standards, We program in languages ​​HTML5, PHP, CSS3, JavaScript, jQuery,
+design from scratch side adapted to the needs of customers and we have been creating WordPress templates.';
 $lang['description'] = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.';
 $lang['ritetoo'] ='Write to us.';
 $lang['name'] ='Name and Surname:';
